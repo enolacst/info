539350cbb143a013e0ebc89894711bf74b2a35ea
GuillaumeRslt <guillaume.rousselet@etu.unilim.fr>
2020-11-24 17:03:31 +0100

    correction pour compilation

--- DM.py
+++ DM.py
@@ -16,14 +16,14 @@ class BoundedOneQueue:
     def __init__(self):
        """création de la file avec priorité bornée n'utilisant qu'une seule liste""" 
     @property
-    def max_priority:
+    def max_priority(self):
         """envoie la valeur utlisée lors de la création de la file"""
         
-    def __len__:
+    def __len__(self):
         """renvoie le nombre d’éléments dans la file"""
         return self.__cpt
     
-    def pop(self):->None
+    def pop(self)->None:
         """enlève le premier élément de plus basse priorité
         require :file non vide"""
         
@@ -32,21 +32,22 @@ class BoundedOneQueue:
         if 0<p<=max_priority :
             return None
         else :
+            return None #return temporaire pour faire passer la complilation
             
-    def first:
+    def first(self):
         """renvoie la première paire (v, p) de la liste
         require : file non vide"""
         
     def empty (self)->bool:
         """revoie vrai si la file est vide, faux sinon"""
 
-    def to_list:
+    def to_list(self):
         """renvoie une liste python des paires (v, p) présentes dans la file"""
 
-    def howmany:
+    def howmany(self):
         """renvoie le nombre d’éléments dans la file ayant cette priorité"""
         
-    def summary:
+    def summary(self):
         """renvoie une liste python de taille max_priority et contenant la distribution des priorités ordonnées de manière croissante"""
 
 class BoundedListQueue:
@@ -54,36 +55,37 @@ class BoundedListQueue:
         """création d'une file avec priorité bornée utilisant une liste, priorité=index"""
         
     @property
-    def max_priority:
+    def max_priority(self):
         """envoie la valeur utlisée lors de la création de la file"""
         
-    def __len__:
+    def __len__(self):
         """renvoie le nombre d’éléments dans la file"""
         
     def pop (slef):
         """enlève le premier élément de plus basse priorité
         require :file non vide"""
         
-    def push:
+    def push(self):
         """permet d'inserer un élément"""
         if p<max-priority:
             return
         else:
+            return None #return temporaire pour faire passer la complilation
             
-    def first:
+    def first(self):
         """renvoie la première paire (v, p) de la liste
         require : file non vide"""
         
     def empty (self)->bool:
         """revoie vrai si la file est vide, faux sinon"""
 
-    def to_list:
+    def to_list(self):
         """renvoie une liste python des paires (v, p) présentes dans la file"""
 
-    def howmany:
+    def howmany(self):
         """renvoie le nombre d’éléments dans la file ayant cette priorité"""
         
-    def summary:
+    def summary(self):
         """renvoie une liste python de taille max_priority et contenant la distribution des priorités ordonnées de manière croissante"""
 
 if __name__ == "__main__":
